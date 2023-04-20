f61d5ce9e955faf72285159a491cd5652cbd68e5
cpprhtn <cpprhtn@naver.com>
2023-04-20 23:32:07 +0900

    mpi_RaceCondition.c

---
+++ mpi_RaceCondition.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <mpi.h>
+
+int main()
+{
+    int size, rank;
+
+    MPI_Init(NULL, NULL);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    printf("Proc %d of %d. \n", rank, size);
+
+    MPI_Finalize();
+    return 0;
+}
